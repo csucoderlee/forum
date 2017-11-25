e3f1ccacfe18423497634a4554669563673e35ab
lixiang <csucoderlee@gmail.com>
2017-11-25 23:55:10 +0800

    增删改查

--- src/main/java/org/csu/controller/PostController.java
+++ src/main/java/org/csu/controller/PostController.java
@@ -1,9 +1,14 @@
 package org.csu.controller;
 
+import org.csu.domain.Post;
 import org.csu.repository.PostRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @author by bixi.lx
@@ -18,6 +23,45 @@ public class PostController {
 
     @GetMapping("/list")
     public Object list() {
-        return postRepository.findAll();
+
+        //todo 按照分类查看帖子
+        //todo 更高完成度，按照当前登录人的喜欢来推荐帖子，推荐相应的分类标签
+        HashMap<String, Object> result = new HashMap<>();
+        result.put("list", postRepository.findAll());
+        result.put("count", postRepository.count());
+        return result;
+    }
+
+    @PostMapping("/save")
+    public Object save(Post post) {
+        //todo 创建帖子或是需要积分， 或是需要管理员审核
+        return postRepository.save(post);
+    }
+
+    /**
+     * 根据帖子的id删除帖子
+     * @param id
+     * @return
+     */
+    @PostMapping("/delete")
+    public Object delete(Long id) {
+        //todo 管理员和作者才能删除帖子
+        if (postRepository.exists(id)) {
+            postRepository.delete(id);
+        } else {
+            throw new IllegalArgumentException("帖子不存在");
+        }
+        return successResponse();
+    }
+
+    @GetMapping("/update")
+    public Object update(String theme, Long id) {
+        return postRepository.modifyById(theme, id);
+    }
+
+    private Map<String, Object> successResponse() {
+        HashMap<String, Object> result = new HashMap<>();
+        result.put("message", "success");
+        return result;
     }
 }
--- src/main/java/org/csu/repository/PostRepository.java
+++ src/main/java/org/csu/repository/PostRepository.java
@@ -2,10 +2,19 @@ package org.csu.repository;
 
 import org.csu.domain.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+
+import javax.transaction.Transactional;
 
 /**
  * @author by bixi.lx
  * @created on 2017 11 25 22:50
  */
 public interface PostRepository extends JpaRepository<Post, Long>{
+
+    @Transactional
+    @Modifying
+    @Query(value = "update Post p set p.theme = ?1 where p.id = ?2")
+    int modifyById(String  theme, Long id);
 }
